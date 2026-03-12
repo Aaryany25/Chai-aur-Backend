0b803935dc0123218e13568c99ea936a6b2fe2c4
Aryan Yadav <aaryany2505@gmail.com>
2026-03-12 21:22:45 +0530

    conneted Frontend with Backend

--- fullstack/frontend/src/App.jsx
+++ fullstack/frontend/src/App.jsx
@@ -3,10 +3,19 @@ import { useState } from 'react'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 // import './App.css'
-
+import axios from 'axios'
+import { useEffect } from 'react'
 function App() {
   const [jokes, setjokes] = useState([])
-
+useEffect(()=>{
+  axios.get('http://localhost:3000/jokes')
+  .then((response)=>{
+    setjokes(response.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+})
   return (
     <>
       <h1>Chai aur Jokes</h1>
